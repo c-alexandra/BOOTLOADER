bc710a25259b50edf5a769593aab10dd302407d3
Camille <camille.alex.aitken@gmail.com>
2025-01-08 14:13:53 -0800

    fix UART

---
+++ bootloader/src/bootloader.d
@@ -0,0 +1,13 @@
+src/bootloader.o: src/bootloader.c \
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/f4/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h \
+ src/../../shared/inc/common.h \
+ /Applications/ArmGNUToolchain/13.3.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/13.3.1/include/stdint.h \
+ /Applications/ArmGNUToolchain/13.3.rel1/arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /Applications/ArmGNUToolchain/13.3.rel1/arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ArmGNUToolchain/13.3.rel1/arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /Applications/ArmGNUToolchain/13.3.rel1/arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ArmGNUToolchain/13.3.rel1/arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ArmGNUToolchain/13.3.rel1/arm-none-eabi/arm-none-eabi/include/sys/_stdint.h \
+ /Applications/ArmGNUToolchain/13.3.rel1/arm-none-eabi/lib/gcc/arm-none-eabi/13.3.1/include/stdbool.h
