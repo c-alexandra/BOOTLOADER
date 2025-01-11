5a84fd9ba95d1d22061ce33f9be7e5dca701710a
Camille Alexandra <camille.alex.aitken@gmail.com>
2025-01-11 13:39:53 -0800

    Init comms files for packet protocol communication

---
+++ bootloader/src/comms.c
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * @file   comms.c
+ * @author Camille Aitken
+ *
+ * @brief
+ ******************************************************************************/
+
+// External library includes
+
+// User includes
+
+// Defines & macros
+
+// Global and Extern Declarations
+
+// Functions
+
+/**
+ * @brief Brief description of the function/element.
+ *
+ * Detailed description of the function/element.
+ * Can include multiple lines and formatting.
+ *
+ * @param param1 Description of the first parameter.
+ * @param param2 Description of the second parameter.
+ * ...
+ * @return Description of the return value.
+ */
